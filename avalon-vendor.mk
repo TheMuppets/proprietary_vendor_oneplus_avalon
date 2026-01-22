9595c771f32a22fe79934309adf075b4d43f18f6
Łukasz Patron <priv.luk@gmail.com>
2026-01-22 22:23:25 +0530

    avalon: Regenerate makefiles
    Change-Id: I0a8ee1d5a6a6f8b5d7179149454e7840696f1e28

--- avalon-vendor.mk
+++ avalon-vendor.mk
@@ -1759,24 +1759,24 @@ PRODUCT_PACKAGES += \
     vendor.pixelworks.hardware.display@1.2 \
     vendor.pixelworks.hardware.feature@1.0 \
     vendor.pixelworks.hardware.feature@1.1 \
-    capi_v3_oprec \
-    libCalculator_skel \
-    libQnnHtpV73 \
-    libQnnHtpV73Skel \
-    libQnnSaver \
-    libSnpeHtpV73Skel \
-    libSuperRawPost_skel \
-    libaisr_skel \
-    libarc_gk_pt_hvx_skel \
-    libarc_htp_driver_skel \
-    libcalculator_skel \
-    libhvxFaceBeautyPre_skel \
-    libktveffect \
-    libtfadsp_sb4_0_rx \
-    libtfadsp_sb4_0_tx \
-    libvdblurless_5.0.2_skel \
-    libvdsuperphoto_skel \
-    nlc_bgth_v1_5 \
+    odm_lib_rfsa_adsp_capi_v3_oprec_so \
+    odm_lib_rfsa_adsp_libCalculator_skel_so \
+    odm_lib_rfsa_adsp_libQnnHtpV73_so \
+    odm_lib_rfsa_adsp_libQnnHtpV73Skel_so \
+    odm_lib_rfsa_adsp_libQnnSaver_so \
+    odm_lib_rfsa_adsp_libSnpeHtpV73Skel_so \
+    odm_lib_rfsa_adsp_libSuperRawPost_skel_so \
+    odm_lib_rfsa_adsp_libaisr_skel_so \
+    odm_lib_rfsa_adsp_libarc_gk_pt_hvx_skel_so \
+    odm_lib_rfsa_adsp_libarc_htp_driver_skel_so \
+    odm_lib_rfsa_adsp_libcalculator_skel_so \
+    odm_lib_rfsa_adsp_libhvxFaceBeautyPre_skel_so \
+    odm_lib_rfsa_adsp_libktveffect_so \
+    odm_lib_rfsa_adsp_libtfadsp_sb4_0_rx_so \
+    odm_lib_rfsa_adsp_libtfadsp_sb4_0_tx_so \
+    odm_lib_rfsa_adsp_libvdblurless_5_0_2_skel_so \
+    odm_lib_rfsa_adsp_libvdsuperphoto_skel_so \
+    odm_lib_rfsa_adsp_nlc_bgth_v1_5_so \
     vendor.qti.camera.aon-impl.xml \
     vendor.qti.camera.offlinecamera-impl.xml \
     vendor.qti.camera.postproc-impl.xml \
